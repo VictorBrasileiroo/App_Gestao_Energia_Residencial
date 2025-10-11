ac06c6ddeec28f36fba406697a125ad18fe6b1f5
johnUFAL <jvdn@ic.ufal.br>
2025-10-11 12:40:35 -0300

    confif adicional para react + preparação para tela de login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm'
+
+//componente funcional da pg de login
+const Login = () => {
+    //retorna o comp LoginForn
+    return <LoginForm />
+}
+
+export default Login
